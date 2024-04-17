e358a1717f99ca698cd23ea2909d19a810d8d6da
Luna-Karch <luna.karch05@gmail.com>
2024-04-17 13:49:32 -0400

    Added JavaDoc for Client class

--- src/main/java/HoyoJava/Client.java
+++ src/main/java/HoyoJava/Client.java
@@ -1,5 +1,10 @@
 package HoyoJava;
 
+/**
+ * Represents a Client, a user who will attempt to connect to the api.
+ * The goal of this class is to make Interacting with the API a little easier
+ * and to manage how a user is allowed to interact with the API
+ */
 public class Client {
-    
+    // TODO
 }
