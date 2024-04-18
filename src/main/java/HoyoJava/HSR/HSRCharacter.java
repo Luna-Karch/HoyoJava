d1019a8e1118ebbf676f29a6202d56850be07a43
Luna-Karch <luna.karch05@gmail.com>
2024-04-18 15:38:36 -0400

    Added Element subclass

--- src/main/java/HoyoJava/HSR/HSRCharacter.java
+++ src/main/java/HoyoJava/HSR/HSRCharacter.java
@@ -20,6 +20,24 @@ public class HSRCharacter {
         public String getIconUrl() { return this.iconUrl; }
     }
 
+    public class Element {
+        private final String ID;
+        private final String name;
+        private final String color;
+        private final String iconUrl;
+
+        public Element(String ID, String name, String color, String iconUrl) {
+            this.ID = ID;
+            this.name = name;
+            this.color = color;
+            this.iconUrl = Client.getActualURL(iconUrl);
+        }
+
+        public String getID() { return this.ID; }
+        public String getName() { return this.name; }
+        public String getColor() { return this.color; }
+        public String getIconUrl() { return this.iconUrl; }
+    }
 
     private final String ID;
     private final String name;
@@ -32,6 +50,7 @@ public class HSRCharacter {
     private String portraitUrl;
     private String[] rankIcons;
     private final Path path;
+    private final Element element;
 
     public HSRCharacter(JsonNode characterNode) {
         throw new UnsupportedOperationException(); // TODO
