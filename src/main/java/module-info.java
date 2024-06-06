7ea0012b5414efc2df5377fc17688a925790871d
Luna Karch <luna.karch05@gmail.com>
2024-06-06 14:49:35 -0400

    Updated dependencies to include javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module HoyoJava {
+    requires transitive javafx.controls;
+    requires javafx.fxml;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+
+    exports HoyoJava;
+}
